61d31158bb41331286d6be5b1d9fb91732c91cf9
John <jmorgan@basecampcodingacademy.org>
2019-04-18 11:47:17 -0500

    Created this class to be used to pick the creature and change the playerCreatureId when selected.

---
+++ src/components/CreatureSelector.js
@@ -0,0 +1,21 @@
+import React, { Component } from "react";
+
+// the selected component in the CreatureSelect bar
+export class CreatureSelector extends Component {
+
+    render(){
+        var className;
+        if(this.props.playerCreatureId === this.props.id){
+            className = "creatureSelect active";
+        }else{
+            className = "creatureSelect";
+        }
+        return(
+            <div className={className} onClick={this.props.onClick}>
+                creat
+            </div>
+        )
+    }
+
+    
+}
