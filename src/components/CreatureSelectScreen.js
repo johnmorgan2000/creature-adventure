d71e938b47b53446c149087270ee888e009e6960
John <jmorgan@basecampcodingacademy.org>
2019-04-22 15:44:43 -0500

    Added button to confirm player creature and changes the phase to fightWaves

--- src/components/CreatureSelectScreen.js
+++ src/components/CreatureSelectScreen.js
@@ -7,10 +7,10 @@ export class CreatureSelectScreen extends Component {
     render() {
         return (
             <div id="creatureSelectScreen">
-                
                 <CreatureSelectDisplay
                 creatureObj={this.props.creatureObj}
                 />
+                <button onClick={() => this.props.changeGamePhase("fightWavesScreen")}>Done</button>
                 <CreatureSelectBar 
                 creatures={this.props.creatures} 
                 playerCreatureId={this.props.playerCreatureId}
