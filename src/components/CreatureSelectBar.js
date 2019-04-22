3dffc608c5177978e672ab4ba5235010704158ca
John <jmorgan@basecampcodingacademy.org>
2019-04-22 12:39:37 -0500

    Added creatureObj prop and id to div.
    The creatureObj prop was added to CreatureSelector.

--- src/components/CreatureSelectBar.js
+++ src/components/CreatureSelectBar.js
@@ -5,17 +5,18 @@ export class CreatureSelectBar extends Component {
 
     render() {
         return (
-            <div>
+            <div id="creatureSelectBar">
                 {this.props.creatures.map((creat, key) => (
                     <CreatureSelector
                         id={creat.id}
+                        creatureObj={creat.creatureObj}
                         key={key}
                         playerCreatureId={this.props.playerCreatureId}
                         onClick={() => this.props.toggleCreatureSelect(creat.id)}
 
                     />
                 ))}
-                {console.log(this.props.playerCreatureId)}
+    
             </div>
         );
     }
