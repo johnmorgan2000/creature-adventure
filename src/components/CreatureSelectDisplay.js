529251ab46194bb87eccb17eaeaee58930cffd98
John <jmorgan@basecampcodingacademy.org>
2019-04-26 13:47:03 -0500

    Removed useless constructor.

--- src/components/CreatureSelectDisplay.js
+++ src/components/CreatureSelectDisplay.js
@@ -2,9 +2,6 @@ import React, { Component } from "react";
 
 // Component to display creature and information
 export class CreatureSelectDisplay extends Component {
-    constructor(props){
-        super(props);
-    }
     render() {
         return (
             <div className="creatureSelectDisplay">
